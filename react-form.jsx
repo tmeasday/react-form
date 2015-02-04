127d4b383fd111b1591c78f2164717746546e17a
Tom Coleman <tom@thesnail.org>
2015-02-04 17:14:17 +1100

    Second step

--- react-form.jsx
+++ react-form.jsx
@@ -3,11 +3,15 @@ var Form = React.createClass({
     return { text: '' };
   },
   
+  handleChange: function(event) {
+    this.setState({text: event.target.value});
+  },
+  
   render: function() {
     return (
       <form>
         <label>Text:</label>
-        <input type="text" name="text" value={this.state.text}/>
+        <input type="text" name="text" value={this.state.text} onChange={this.handleChange}/>
       </form>
     )
   }
