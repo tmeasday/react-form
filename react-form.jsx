fc7c8bfab82060e1006cc3af089348134b134de9
Tom Coleman <tom@thesnail.org>
2015-02-04 17:18:23 +1100

    Added remaining logic

--- react-form.jsx
+++ react-form.jsx
@@ -1,3 +1,5 @@
+var MAX_CHARS=20;
+
 var Form = React.createClass({
   getInitialState: function() {
     return { text: '' };
@@ -7,11 +9,16 @@ var Form = React.createClass({
     this.setState({text: event.target.value});
   },
   
+  remaining: function() {
+    return MAX_CHARS - this.state.text.length;
+  },
+  
   render: function() {
     return (
       <form>
         <label>Text:</label>
         <input type="text" name="text" value={this.state.text} onChange={this.handleChange}/>
+        <p>{this.remaining()} chars to go</p>
       </form>
     )
   }
